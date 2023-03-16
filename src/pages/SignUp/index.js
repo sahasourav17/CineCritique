1215e1da3ff3ee04694a92483b1f3da996f7aae0
sahasourav17 <sahasourav1170@gmail.com>
2023-03-16 17:51:16 +0600

    hit signup endpoint and add style

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -19,29 +19,72 @@ const SignUp = () => {
           password: passwordRef.current.value,
         })
       );
-      console.log(response.data.status);
+      console.log(response);
     } catch (err) {
-      console.log("Some error occured during signing up: ", err.message);
+      console.log("Some error occured during signing up: ", err);
     }
   };
   return (
     <>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="Id">id</label>
-          <input ref={idRef} type="number" id="id" />
-          <br />
-          <label htmlFor="name">Name</label>
-          <input ref={nameRef} type="text" id="name" />
-          <br />
-          <label htmlFor="email">Email</label>
-          <input ref={emailRef} type="email" id="email" />
-          <br />
-          <label htmlFor="password">Password</label>
-          <input ref={passwordRef} type="password" id="password" />
-          <br />
-          <button type="submit">Sign Up</button>
-        </form>
+      <div className="flex h-screen w-full items-center">
+        <div className="m-4 w-full rounded bg-white p-8 shadow-lg md:mx-auto md:max-w-sm">
+          <span className="item-center mb-4 block w-full text-xl font-bold">
+            CineCritique
+          </span>
+
+          <form className="mb-4" onSubmit={handleSubmit}>
+            <label className="mb-1 block text-xs" htmlFor="Id">
+              Id
+            </label>
+            <input
+              className="focus:shadow-outline w-full rounded border p-1.5 text-xs outline-none"
+              ref={idRef}
+              type="number"
+              id="id"
+            />
+            <br />
+
+            <label className="mb-1 block text-xs" htmlFor="name">
+              Name
+            </label>
+            <input
+              className="focus:shadow-outline w-full rounded border p-1.5 text-xs outline-none"
+              ref={nameRef}
+              type="text"
+              id="name"
+            />
+            <br />
+
+            <label className="mb-1 block text-xs" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="focus:shadow-outline w-full rounded border p-1.5 text-xs outline-none"
+              ref={emailRef}
+              type="email"
+              id="email"
+            />
+            <br />
+
+            <label className="mb-1 block text-xs" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="focus:shadow-outline mb-2 w-full rounded border p-1.5 text-xs outline-none"
+              ref={passwordRef}
+              type="password"
+              id="password"
+            />
+            <br />
+
+            <button
+              className="rounded bg-green-500 px-2.5 py-1 text-sm  uppercase text-white hover:bg-green-700"
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
