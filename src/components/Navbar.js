c225aa0ed6d78e8c223ac4db398af0c135d33778
sahasourav17 <sahasourav1170@gmail.com>
2023-03-22 20:07:23 +0600

    add placeholder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -65,7 +65,7 @@ const Navbar = () => {
             to="/signin"
             onClick={handleLogOut}
           >
-            LogOut
+            Logout
           </NavLink>
         </div>
       </header>
