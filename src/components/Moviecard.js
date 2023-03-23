868368881636486137c67d70ff5531d5a768a0d9
sahasourav17 <sahasourav1170@gmail.com>
2023-03-23 17:34:19 +0600

    add search functionality

---
+++ src/components/Moviecard.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Moviecard = ({ data }) => {
+  return (
+    <>
+      <div className="pt -4 flex--movie align-center flex flex-wrap justify-center">
+        <div className="m-4 flex w-full max-w-4xl justify-between overflow-hidden rounded shadow-lg md:w-full lg:w-1/2">
+          <div className="md:flex-shrink-0">
+            <img className="md:w-56" src={data.Poster} alt={data.Title} />
+          </div>
+          <div className="bg-color-333 flex flex-grow flex-col px-8 py-4">
+            <h3 className="movie--title text-4xl font-bold text-rainblue-100 md:text-2xl lg:text-2xl">
+              {data.Title}
+            </h3>
+            <span className="movie--year text-xl lg:mb-3 lg:text-sm">
+              {data.Year}
+            </span>
+            <div className="flex-grow">
+              <span className="text-black font-semibold">Rating: </span>
+              <span className="movie--year text-lg lg:mb-3 lg:text-sm">
+                {data.imdbRating}/10
+              </span>
+              <p className="truncate-overflow text-black mb-2 text-xl leading-snug md:text-base lg:text-base">
+                {data.Actors}
+              </p>
+              <p className="truncate-overflow text-black mb-2 text-xl leading-snug md:text-base lg:text-base">
+                {data.Plot}
+              </p>
+              <span className="text-black font-semibold">Genre: </span>
+              <span className="text-rainblue-50">{data.Genre}</span>
+            </div>
+            <div className="button-container mb-2 flex justify-between">
+              <button className="text-medium rounded bg-rainblue-50 py-2 px-4 font-semibold text-white lg:text-sm">
+                More Info
+              </button>
+              <button className="text-medium rounded bg-rainblue-50 py-2 px-4 font-semibold text-white lg:text-sm">
+                Review Movie
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Moviecard;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Footer } from "../../components/index";
+import Moviecard from "../../components/Moviecard";
+import { publicGet } from "../../utilities/apiCaller";
 import SignIn from "../SignIn";
 
 const Home = () => {
+  const [movieName, setMovieName] = useState("");
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const token = sessionStorage.getItem("token");
   let isAuthenticated = false;
   if (token !== null) {
     isAuthenticated = true;
   }
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      setIsLoading(true);
+      const endpoint = "/movie/name/" + movieName;
+      const response = await publicGet(endpoint);
+      if (response.data.status === 200) {
+        setData(response.data.data);
+      }
+    } catch (err) {
+      alert("Movie not found!");
+    }
+    setIsLoading(false);
+    setMovieName("");
+  };
   return (
     <>
       {isAuthenticated ? (
@@ -17,7 +38,35 @@ const Home = () => {
             <h1 className="my-6 text-center text-3xl font-bold text-white">
               Home
             </h1>
+            <form
+              className=" flex items-center justify-center p-2"
+              onSubmit={handleSearch}
+            >
+              <input
+                className="text-medium border-b border-b-white bg-rainblue-100 placeholder-white caret-white focus:outline-none"
+                type="text"
+                placeholder="Type movie name"
+                value={movieName}
+                onChange={(e) => setMovieName(e.target.value)}
+              />
+              <button className="bg-white pr-2 pl-2 font-bold uppercase">
+                Search
+              </button>
+            </form>
           </div>
+          {data && <Moviecard data={data} />}
+          {isLoading && (
+            <div className="flex items-center justify-center pt-20">
+              <div
+                className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                role="status"
+              >
+                <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                  Loading...
+                </span>
+              </div>
+            </div>
+          )}
           <Footer />
         </>
       ) : (
